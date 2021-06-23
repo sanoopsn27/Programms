f57a0cb53ca004d08084e854f7c6ee0d4e4dd22c
LENOVO <LENOVO@DESKTOP-PMQ1580>
2021-06-23 19:08:25 +0530

    asteroid collision

--- Programms/src/astroid_collision.java
+++ Programms/src/astroid_collision.java
@@ -5,7 +5,7 @@ public class astroid_collision {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int arr_size=0;int i,j,height_array_size,large=0;
+		int arr_size=0;int i,j,result_top;
 		
 		Scanner input=new Scanner(System.in);
 		
@@ -13,31 +13,73 @@ public class astroid_collision {
 		
 		arr_size=input.nextInt();
 		
-		int inarr[]=new int[arr_size];
-		int outarr[]=new int[arr_size];
+		int astr[]=new int[arr_size];
+		int result[]=new int[arr_size];
 		
 		System.out.println("Enter values");
 		
 		for(i=0;i<arr_size;i++)
 		{
-			inarr[i]=input.nextInt();
+			astr[i]=input.nextInt();
 			
 		}
 		
-		for(i=0;i<arr_size;i++)
-		{
-			System.out.println(inarr[i]);
-		}
 		
-		for(i=0,j=arr_size;i<arr_size;i++)
+		for(i=0,j=-1;i<arr_size;i++)
 		{
 		
-			if( inarr[i] < inarr[j]  )
-			{
-				outarr[i]=Math.abs(inarr[j]);
-			}
+			 if((result.length==0) || astr[i]>0)
+			 {		
+				 j++;
+				 result[j]=astr[i];
+			 }
+			 else 
+			 {
+				 while(true)
+				 {
+					 result_top=result[j];
+					 
+					 if(result_top < 0)
+					 {
+						 j++;
+						 result[j]=astr[i];
+						 break;
+					 }
+					 else if (result_top==-astr[i])
+					 {
+						j--;
+						break;
+							 
+					 }
+					 else if(result_top> - astr[i])
+					{
+						break; 
+					}
+					 else
+					 {
+						 j--;
+						 
+						 if(j==0)
+						 {
+							 j++;
+							 result[j]=astr[i];
+							 break;
+						 }
+					 }
+				 
+				 
+			 }
+			 
+				 
+			 }
 			
 		}
+		
+		for(i=0;i<=j;i++)
+		{
+			System.out.println(result[i]);
+		}
+		
 
 		
 
